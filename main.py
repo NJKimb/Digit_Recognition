4102976a6fb2809ab5c4b192ee99b28bfe3acd37
Noah Kimbler <kimbl056@d.umn.edu>
2024-03-21 17:24:23 -0500

    Changed the MLP network to a CNN for better accuracy and changed training mode to a command line argument

--- main.py
+++ main.py
@@ -1,34 +1,66 @@
-import os
+import sys
 import torch
 from torch import nn
+from torch.nn import MaxPool2d, LogSoftmax
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+
+
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
-        )
+
+        # First layer of convolution (Conv -> RelU -> Max Pool)
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = MaxPool2d(kernel_size=2, stride=2)
+
+        # Second layer of convolution (Conv -> RelU -> Max Pool)
+        self.conv2 = nn.Conv2d(10, 50, kernel_size=5)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = MaxPool2d(kernel_size=2, stride=2)
+
+        # First layer of linear units ( Linear -> RelU)
+        self.linear1 = nn.Linear(800, 512)
+        self.relu3 = nn.ReLU()
+
+        # Ouput layer of NN ( Linear -> SoftMax)
+        self.linear2 = nn.Linear(512, 10)
+        self.softmax = LogSoftmax(dim=1)
 
     def forward(self, x):
-        x = x.view(-1, 28*28)
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        # First Layer
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+
+        # Second Layer
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+
+        # Flatten then 3rd layer
+        x = torch.flatten(x, 1)
+        x = self.linear1(x)
+        x = self.relu3(x)
+
+        # Output Layer
+        x = self.linear2(x)
+        logits = self.softmax(x)
+
         return logits
+
 
 class DeviceDataLoader:
     def __init__(self, dl, device):
         self.dl = dl
         self.device = device
+
     def __iter__(self):
         for batch in self.dl:
             yield to_device(batch, self.device)
+
     def __len__(self):
         return len(self.dl)
 
@@ -38,7 +70,9 @@ def to_device(data, device):
         return [to_device(x, device) for x in data]
     return data.to(device, non_blocking=True)
 
+
 def train_network():
+    # Use GPU if available
     device = (
         "cuda" if torch.cuda.is_available()
         else "cpu"
@@ -48,9 +82,10 @@ def train_network():
     model = NeuralNetwork().to(device)
 
     transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.Normalize((0.5), (0.5))])
+                                    transforms.Normalize((0.5,), (0.5,))])
+
     trainset = datasets.MNIST(root="./data", train=True,
-                             download=True,transform=transform)
+                              download=True, transform=transform)
     train_loader = DataLoader(trainset, batch_size=10, shuffle=True)
 
     testset = datasets.MNIST(root="./data", train=False,
@@ -61,46 +96,48 @@ def train_network():
     test_loader = DeviceDataLoader(test_loader, device)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
-    for epoch in range(10):
+    # Training loop
+    for epoch in range(25):
         model.train()
 
-        for data in train_loader:
-            X, y = data
+        for (X, y) in train_loader:
             optimizer.zero_grad()
-            output = model(X.view(-1, 28*28))
-            loss = criterion(output, y)
+            pred = model(X)
+            loss = criterion(pred, y)
             loss.backward()
             optimizer.step()
 
     correct_predictions = 0
     total_predictions = 0
 
+    # Validation loop
     with torch.no_grad():
-        for data in test_loader:
-            X, y = data
-            output = model(X.view(-1, 784))
-            for index, i in enumerate(output):
-                if torch.argmax(i) == y[index]:
-                    correct_predictions += 1
-                total_predictions += 1
+        model.eval()
 
-    torch.save(model.state_dict(), "dict")
-    print(f"Accuracy: {round(correct_predictions/total_predictions, 3)}")
+        for data in test_loader:
+            images, labels = data
+            pred = model(images)
+            predicted = torch.max(pred.data, 1)[1]
+            correct_predictions += (predicted == labels).sum().item()
+            total_predictions += labels.size(0)
+
+    accuracy = correct_predictions / total_predictions
+    torch.save(model.state_dict(), "dict.pth")
+    print(f"Accuracy: {accuracy}")
 
 
 def detect():
     model = NeuralNetwork()
-    model.load_state_dict(torch.load("dict"))
+    model.load_state_dict(torch.load("dict.pth"))
     model.eval()
 
     print(model)
 
-if __name__ == "__main__":
-    choice = input("Ender \"t\" for training or \"d\" for detection: ")
 
-    if choice == "t":
+if __name__ == "__main__":
+    if sys.argv[1] == '-t':
         train_network()
-    elif choice == "d":
+    else:
         detect()

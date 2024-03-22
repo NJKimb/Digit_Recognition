fb83e0f706ed0c3e0f8d788d8ca9e53b9b17a311
Noah Kimbler <kimbl056@d.umn.edu>
2024-03-21 20:57:53 -0500

    Can now detect digits in .jpg images supplied via command line arguments

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
 import sys
+import cv2
 import torch
+from PIL import Image
 from torch import nn
 from torch.nn import MaxPool2d, LogSoftmax
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+import numpy
 
+# Hyperparameters
+LearningRate = 0.001
+BatchSize = 10
+Epochs = 10
+DropoutRate = .3
 
-
+# Neural Network class
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
 
-        # First layer of convolution (Conv -> RelU -> Max Pool)
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        # First layer of convolution (Conv -> RelU -> Max Pool -> Dropout)
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5, padding=1)
         self.relu1 = nn.ReLU()
         self.maxpool1 = MaxPool2d(kernel_size=2, stride=2)
+        self.dropout1 = nn.Dropout(DropoutRate)
 
-        # Second layer of convolution (Conv -> RelU -> Max Pool)
-        self.conv2 = nn.Conv2d(10, 50, kernel_size=5)
+        # Second layer of convolution (Conv -> RelU -> Max Pool -> Dropout)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5, padding=1)
         self.relu2 = nn.ReLU()
         self.maxpool2 = MaxPool2d(kernel_size=2, stride=2)
+        self.dropout2 = nn.Dropout(DropoutRate)
 
-        # First layer of linear units ( Linear -> RelU)
-        self.linear1 = nn.Linear(800, 512)
+        # First layer of linear units ( Linear -> RelU -> Dropout)
+        self.linear1 = nn.Linear(500, 256)
         self.relu3 = nn.ReLU()
+        self.dropout3 = nn.Dropout(DropoutRate)
 
-        # Ouput layer of NN ( Linear -> SoftMax)
-        self.linear2 = nn.Linear(512, 10)
+        # Output layer of NN ( Linear -> SoftMax)
+        self.linear2 = nn.Linear(256, 10)
         self.softmax = LogSoftmax(dim=1)
 
     def forward(self, x):
@@ -34,16 +45,19 @@ class NeuralNetwork(nn.Module):
         x = self.conv1(x)
         x = self.relu1(x)
         x = self.maxpool1(x)
+        x = self.dropout1(x)
 
         # Second Layer
         x = self.conv2(x)
         x = self.relu2(x)
         x = self.maxpool2(x)
+        x = self.dropout2(x)
 
-        # Flatten then 3rd layer
+        # Flatten then third layer
         x = torch.flatten(x, 1)
         x = self.linear1(x)
         x = self.relu3(x)
+        x = self.dropout3(x)
 
         # Output Layer
         x = self.linear2(x)
@@ -52,6 +66,7 @@ class NeuralNetwork(nn.Module):
         return logits
 
 
+# Dataloader to send data to GPU
 class DeviceDataLoader:
     def __init__(self, dl, device):
         self.dl = dl
@@ -65,6 +80,7 @@ class DeviceDataLoader:
         return len(self.dl)
 
 
+# Utility function for moving data to GPU
 def to_device(data, device):
     if isinstance(data, (list, tuple)):
         return [to_device(x, device) for x in data]
@@ -81,25 +97,26 @@ def train_network():
 
     model = NeuralNetwork().to(device)
 
+    # Download and transform MNIST dataset
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize((0.5,), (0.5,))])
 
     trainset = datasets.MNIST(root="./data", train=True,
                               download=True, transform=transform)
-    train_loader = DataLoader(trainset, batch_size=10, shuffle=True)
+    train_loader = DataLoader(trainset, batch_size=BatchSize, shuffle=True)
 
     testset = datasets.MNIST(root="./data", train=False,
                              download=True, transform=transform)
-    test_loader = DataLoader(testset, batch_size=10, shuffle=True)
+    test_loader = DataLoader(testset, batch_size=BatchSize, shuffle=True)
 
     train_loader = DeviceDataLoader(train_loader, device)
     test_loader = DeviceDataLoader(test_loader, device)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
+    optimizer = torch.optim.SGD(model.parameters(), lr=LearningRate)
 
     # Training loop
-    for epoch in range(50):
+    for epoch in range(Epochs):
         model.train()
 
         for (X, y) in train_loader:
@@ -129,11 +146,29 @@ def train_network():
 
 
 def detect():
+    # Initialize and load neural network
     model = NeuralNetwork()
     model.load_state_dict(torch.load("dict.pth"))
     model.eval()
 
-    print(model)
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5,), (0.5,))])
+
+    # Transform image to MNIST format
+    image = Image.open(sys.argv[1]).convert('L')
+    image = image.resize((28, 28))
+    image = cv2.bitwise_not(numpy.asarray(image))
+
+    # Transform to tensor
+    image = transform(image)
+
+    # Predict with neural network
+    with torch.no_grad():
+        pred = model(image.unsqueeze(0))
+        predicted = torch.max(pred.data, 1)[1].item()
+        class_labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        predicted_label = class_labels[predicted]
+        print(f"Predicted label: {predicted_label}")
 
 
 if __name__ == "__main__":
